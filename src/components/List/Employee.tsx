36c4e7b7eface36e86a6f6d5042ccd100fe06944
Sebastien Stordeur <stordeursebastien0@gmail.com>
2022-09-07 09:33:05 +0200

    fix localStorage TS Issue

--- src/components/List/Employee.tsx
+++ src/components/List/Employee.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 
-const Employee: React.FC = () => {
+interface IEmployee {
+  employee: {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    startDate: string;
+    department: string;
+    street: string;
+    city: string;
+    state: string;
+    zipCode: string;
+  };
+}
+
+const Employee: React.FC<IEmployee> = (props) => {
+  console.log("props", props);
   return (
     <div className="odd:bg-grey even:bg-light-grey grid grid-cols-9 h-10 py-1.5 text-center">
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
-      <span>test</span>
+      <span>{props.employee.firstName}</span>
+      <span>{props.employee.lastName}</span>
+      <span>{props.employee.startDate}</span>
+      <span>{props.employee.department}</span>
+      <span>{props.employee.dateOfBirth}</span>
+      <span>{props.employee.street}</span>
+      <span>{props.employee.city}</span>
+      <span>{props.employee.state}</span>
+      <span>{props.employee.zipCode}</span>
     </div>
   );
 };
--- src/components/List/EmployeesList.tsx
+++ src/components/List/EmployeesList.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Employee from "./Employee";
 
+type Employee = {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  startDate: string;
+  department: string;
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+};
+
 const EmployeesList: React.FC = () => {
+  const [employees, setEmployees] = useState<Employee[] | any>([]);
+
+  const storedEmployees = localStorage.getItem("employees");
+
+  useEffect(() => {
+    if (typeof storedEmployees === "string") {
+      let localEmployees: Employee | null;
+      localEmployees = JSON.parse(storedEmployees);
+      setEmployees(localEmployees);
+    }
+  }, [storedEmployees]);
+
   return (
     <section id="employee-list" className="max-w-[1110px] mx-auto mt-5 ">
       <div className="grid grid-cols-9 text-white bg-blue h-10 font-medium text-center py-1.5">
@@ -16,9 +40,19 @@ const EmployeesList: React.FC = () => {
         <span>Zip Code</span>
       </div>
       <div>
-        <Employee />
-        <Employee />
-        <Employee />
+        {employees.slice(0, 10).map((employee: Employee) => {
+          return (
+            <Employee key={Math.random().toString()} employee={employee} />
+          );
+        })}
+      </div>
+      <div className="flex justify-between">
+        <p>Showing X of X</p>
+        <div>
+          <button>Previous</button>
+          <span className="mx-4">1</span>
+          <button>Next</button>
+        </div>
       </div>
     </section>
   );
